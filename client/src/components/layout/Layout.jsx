b9fa4400eeee2d3b57421cadac108f4da52de94d
Sudip Kumar Prasadmkdir <sudipkumarprasad2005@gmail.com>
2026-03-24 00:15:58 +0530

    UI Redesign: Soften global styles and increase layout breathability

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -4,14 +4,14 @@ import Topbar from './Topbar'
 
 export default function Layout() {
   return (
-    <div className="flex h-screen w-screen p-6 md:p-10 overflow-hidden dot-grid bg-[#0a0a0a]">
+    <div className="flex h-screen w-screen p-4 md:p-8 lg:p-12 overflow-hidden dot-grid bg-black">
       <div 
-        className="flex h-full w-full rounded-[2.5rem] border border-[#1f1f22] overflow-hidden shadow-[0_20px_100px_rgba(0,0,0,0.8)] bg-[#0a0a0a] relative" 
+        className="flex h-full w-full rounded-[3rem] border border-archivist-border overflow-hidden shadow-[0_40px_120px_-20px_rgba(0,0,0,0.9)] bg-archivist-dark relative" 
       >
         <Sidebar />
         <div className="flex flex-col flex-1 overflow-hidden">
           <Topbar />
-          <main className="flex-1 overflow-y-auto p-12 lg:p-16 custom-scrollbar">
+          <main className="flex-1 overflow-y-auto p-16 lg:p-24 custom-scrollbar">
             <Outlet />
           </main>
         </div>
